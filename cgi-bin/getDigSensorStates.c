9e8c42b19b3b19c3c3329137fecbd1171b46b570
Ian McGaunn <ianmcgaunn@Ians-MacBook-Air.local>
2013-04-11 18:17:27 -0400

    remove and ignore all .cgi files

--- cgi-bin/getDigSensorStates.c
+++ cgi-bin/getDigSensorStates.c
@@ -1,16 +1,16 @@
-int main() 
+int main()
 {
-	int i;
-	
-	for (i=8; i < 16; i++){
-		if(i < 10) {
-			printf("<strong>%d</strong>  %d", i, digital(i));
-			printf("<br />");
-		}
-		else {
-			printf("<strong>%d</strong> %d", i, digital(i));
-			printf("<br />");	
-		}
-	}
-	return 0;
+  int i;
+
+  for (i=8; i < 16; i++){
+    if(i < 10) {
+      printf("<strong>%d</strong>  %d", i, digital(i));
+      printf("<br />");
+    }
+    else {
+      printf("<strong>%d</strong> %d", i, digital(i));
+      printf("<br />");
+    }
+  }
+  return 0;
 }
